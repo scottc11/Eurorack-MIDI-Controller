df050e9104da5597352343cb0834dd62aed432c7
Scott Campbell <scottcampbell@MacBooks-MacBook-Pro.local>
2019-01-27 09:37:30 -0500

    tried adding fourth encoder but didn't work.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <Arduino.h>
 #include <MIDI.h>
 #include <TimerOne.h>
+
+#define ENCODER_DO_NOT_USE_INTERRUPTS // so Encoder library doesn't use interupts
 #include <Encoder.h>
 #include <Adafruit_MCP23017.h>
 
@@ -14,21 +16,29 @@ Adafruit_MCP23017 mcp = Adafruit_MCP23017();
 #define MIDI_START 0xFA
 #define MIDI_STOP  0xFC
 
+
 Encoder encoder1(9, 12);
 Encoder encoder2(8, 11);
 Encoder encoder3(7, 10);
+// Encoder encoder4(6, 13);
+
 
 int MIDI_CHANNEL = 1;
-bool DEBUG = true;
+bool DEBUG = false;
 
 
 bool started = false;
 
 // Trigger Input Pins
-int inputCount = 3;
-int inputPins[] = {2, 4, 5};
-int buttonMIDIValues[] = {21, 37, 53};
-int counter[] = {0, 0, 0}; // for encoder ?
+int triggerInputCount = 4;
+int trigger_input_pins[] = {8, 9, 10, 11};  // *** MCP23017 pins
+
+int midi_start_pin = 4;
+
+int buttonMIDIValues[] = {21, 37, 53, 77};
+
+
+int counter[] = {0, 0, 0, 0}; // for encoder ?
 
 // button states
 byte newButtonStates = 0;
@@ -37,6 +47,7 @@ byte oldButtonStates = 0;
 long oldPosition1 = -999;
 long oldPosition2 = -999;
 long oldPosition3 = -999;
+// long oldPosition4 = -999;
 
 int step = 0;
 
@@ -80,29 +91,30 @@ void setup() {
     Serial.begin(9600);
   }
 
+  // DIGITAL PIN 3 is used for detecting tempo.
   attachInterrupt(digitalPinToInterrupt(3), sendClockPulse, FALLING);
 
-  // mcp.begin();
 
-  // Connect MCP23017 pinouts
-  // mcp.pinMode(8, INPUT);
+  pinMode(midi_start_pin, INPUT); // MIDI START button
+  pinMode(5, INPUT); // MIDI STOP button
 
-  // BUTTONS
-  pinMode(2, INPUT);
-  pinMode(4, INPUT);
-  pinMode(5, INPUT);
-  pinMode(6, INPUT);
+  // INIT TRIGGER INPUTS
+  mcp.begin();
+  for (size_t i = 0; i < triggerInputCount; i++) {
+    mcp.pinMode(trigger_input_pins[i], INPUT);
+  }
 }
 
 void loop() {
 
-  if (digitalRead(6) == HIGH && !started) {
+  if (digitalRead(midi_start_pin) == HIGH && !started) {
     startClock();
   }
 
   long newPosition1 = encoder1.read();
   long newPosition2 = encoder2.read();
   long newPosition3 = encoder3.read();
+  // long newPosition4 = encoder4.read();
 
   // ENCODER ONE
   if (newPosition1 != oldPosition1 ) {
@@ -146,18 +158,32 @@ void loop() {
     oldPosition3 = newPosition3;
   }
 
+  // ENCODER FOUR
+  // if (newPosition4 != oldPosition4 ) {
+  //   if (DEBUG) { Serial.println(newPosition4); }
+  //
+  //   if (newPosition4 % 4 == 0) {
+  //     if (oldPosition4 > newPosition4) {
+  //       counter[3] += 1;
+  //     } else {
+  //       counter[3] -= 1;
+  //     }
+  //   }
+  //   oldPosition4 = newPosition4;
+  // }
+
 
 
 
   // get switch states of each scale step
-  for (uint8_t i=0; i < inputCount; i++) {
-    uint8_t state = digitalRead(inputPins[i]);
+  for (uint8_t i=0; i < triggerInputCount; i++) {
+    uint8_t state = mcp.digitalRead(trigger_input_pins[i]);
     bitWrite(newButtonStates, i, state);
   }
 
   if ( newButtonStates != oldButtonStates ) {
 
-    for (uint8_t i=0; i < inputCount; i++) {
+    for (uint8_t i=0; i < triggerInputCount; i++) {
 
       if (bitRead(newButtonStates, i) != bitRead(oldButtonStates, i)) {
         if (bitRead(newButtonStates, i) == LOW) {
